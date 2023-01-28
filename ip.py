04d7e9da2b28578ebe5fc029625be29f4a55feee
ThVODKA <109770727+ThVODKA@users.noreply.github.com>
2023-01-29 00:28:34 +0300

    Add files via upload

---
+++ ip.py
@@ -0,0 +1,7 @@
+import socket
+check = input("IP adresinizi öğrenmek istiyor musunuz? (y/n): ")
+if check == 'n':    
+   exit()
+else:    
+  print("\nIP adresiniz: ",end="")
+  print(socket.gethostbyname(socket.gethostname()))
